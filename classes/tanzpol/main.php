2f3e290257ff021289f38db7abc873139178aaef
Balancer <balancer@balancer.ru>
2015-05-25 04:34:50 +0300

    Временное статкеширование.

--- classes/tanzpol/main.php
+++ classes/tanzpol/main.php
@@ -8,85 +8,89 @@ class tanzpol_main extends tanzpol_page
 
 	function create_time() { return 1343759827; }
 
+	function cache_static() { return 600; }
+
 	static function cat_names() { return "tanzpol"; }
 
 	function body_data()
 	{
+		$data = json_decode(blib_http::get_cached('http://www.balancer.ru/rpc/json/tanzpol', 600), true);
+
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=новости&fields=body,title,snip&thumb=550x330(up,crop)&img=550&limit=10';
-		$big_pics = json_decode(file_get_contents($url), true);
+		$big_pics = json_decode(blib_http::get_cached($url, 600), true);
 
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=новости&fields=body,title,snip&thumb=567x330(up,crop)&img=567&limit=3';
-		$big_pics_mid = json_decode(file_get_contents($url), true);
+		$big_pics_mid = json_decode(blib_http::get_cached($url, 600), true);
 
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=новости&fields=body,title,snip&thumb=300x215(up,crop)&limit=4'
 			.'&id_not='.join(',', array_keys($big_pics));
-		$top_news = json_decode(file_get_contents($url), true);
+		$top_news = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_keys($top_news);
 
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=Россия/новости&fields=body,title,snip&thumb=390x240(up,crop)&limit=1'
 			.'&id_not='.join(',', array_keys($used_keys));
-		$russia_news1 = json_decode(file_get_contents($url), true);
+		$russia_news1 = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($russia_news1));
 		$russia_news1 = array_pop($russia_news1);
 
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=Россия/новости&fields=body,title,snip&thumb=112x112(up,crop)&limit=3'
 			.'&id_not='.join(',', $used_keys);
-		$russian_news = json_decode(file_get_contents($url), true);
+		$russian_news = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($russian_news));
 
 /*		Украина */
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=Украина/новости&fields=body,title,snip&thumb=390x240(up,crop)&limit=1'
 			.'&id_not='.join(',', array_keys($used_keys));
-		$ua_news1 = json_decode(file_get_contents($url), true);
+		$ua_news1 = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($ua_news1));
 		$ua_news1 = array_pop($ua_news1);
 
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=Украина/новости&fields=body,title,snip&thumb=112x112(up,crop)&limit=2'
 			.'&id_not='.join(',', $used_keys);
-		$ua_news = json_decode(file_get_contents($url), true);
+		$ua_news = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($ua_news));
 
 /*		Происшествия */
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=происшествия,события,аварии,катастрофы,стихийные+бедствия&fields=body,title,snip&thumb=390x240(up,crop)&limit=1'
 			.'&id_not='.join(',', array_keys($used_keys));
-		$event_news1 = json_decode(file_get_contents($url), true);
+		$event_news1 = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($event_news1));
 		$event_news1 = array_pop($event_news1);
 
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=происшествия,события,аварии,катастрофы,стихийные+бедствия&fields=body,title,snip&thumb=112x112(up,crop)&limit=2'
 			.'&id_not='.join(',', $used_keys);
-		$event_news = json_decode(file_get_contents($url), true);
+		$event_news = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($event_news));
 
 /*		Общество, экономика */
 		$tags='новости/общество,экономика,социум,протесты';
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags='.$tags.'&fields=body,title,snip&thumb=390x240(up,crop)&limit=1'
 			.'&id_not='.join(',', array_keys($used_keys));
-		$social_news1 = json_decode(file_get_contents($url), true);
+		$social_news1 = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($social_news1));
 		$social_news1 = array_pop($social_news1);
 
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags='.$tags.'&fields=body,title,snip&thumb=112x112(up,crop)&limit=3'
 			.'&id_not='.join(',', $used_keys);
-		$social_news = json_decode(file_get_contents($url), true);
+		$social_news = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($social_news));
 
 /*		Флот */
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=новости/флот&fields=body,title,snip&thumb=292x150(up,crop)&limit=2'
 			.'&id_not='.join(',', $used_keys);
-		$navy_news = json_decode(file_get_contents($url), true);
+		$navy_news = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($navy_news));
 
 /*		Авиация */
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=новости/авиация&fields=body,title,snip&thumb=292x150(up,crop)&limit=2'
 			.'&id_not='.join(',', $used_keys);
-		$avia_news = json_decode(file_get_contents($url), true);
+		$avia_news = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($avia_news));
 
 /* Прочее */
 		$url = 'http://www.balancer.ru/rpc/json/find/balancer_board_post?tags=новости&fields=body,title,snip&thumb=112x112(up,crop)&limit=3'
 			.'&id_not='.join(',', $used_keys);
-		$latest_news = json_decode(file_get_contents($url), true);
+		$latest_news = json_decode(blib_http::get_cached($url, 600), true);
 		$used_keys = array_merge($used_keys, array_keys($latest_news));
 
 		return array_merge(parent::body_data(), compact('avia_news', 'big_pics', 'big_pics_mid', 'event_news', 'event_news1',
@@ -94,7 +98,7 @@ class tanzpol_main extends tanzpol_page
 			'social_news1', 'social_news',
 			'ua_news', 'ua_news1', 'top_news'), array(
 			'asset_path' => '/magexpress',
-		), json_decode(file_get_contents('http://www.balancer.ru/rpc/json/tanzpol'), true));
-//		return json_decode(file_get_contents('http://www.wrk.ru/news/tags/политика/last.json'), true);
+		), $data);
+//		return json_decode(blib_http::get_cached('http://www.wrk.ru/news/tags/политика/last.json', 600), true);
 	}
 }
--- config.php
+++ config.php
@@ -5,4 +5,10 @@ config_set('classes_auto_base', 'tanzpol');
 config_set('jquery.use_cdn', true);
 config_set('jquery.version', '2.1.1');
 
+config_set('is_developer', true);
+config_set('debug.timing', true);
+
+config_set('cache_static', true);
+config_set('cache_static.root', '/var/www/tanzpol.works.home.balancer.ru/htdocs/cache-static');
+
 // require_once(__DIR__.'/config-host.php');
